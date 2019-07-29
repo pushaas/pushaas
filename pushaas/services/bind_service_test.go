909a243069cc9fd1056c2ef525b6eb8f503125b1
rafaeleyng <rafaeleyng@gmail.com>
2019-07-28 22:55:14 -0300

    write tests for bind service - unbind method

--- pushaas/services/bind_service_test.go
+++ pushaas/services/bind_service_test.go
@@ -16,9 +16,9 @@ import (
 var _ = Describe("BindService", func() {
 	config := viper.New()
 	instanceName := "instance-1"
-	appBindForm := &models.BindAppForm{
-
-	}
+	appBindForm := &models.BindAppForm{}
+	appName := "app-1"
+	appHost := "app-host-1"
 
 	_ = Describe("BindApp", func() {
 		_ = It("indicates when instance is not found", func() {
@@ -98,8 +98,8 @@ var _ = Describe("BindService", func() {
 			redisClient := &mocks.UniversalClientMock{
 				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
 					return redis.NewStringStringMapResult(map[string]string {
-						"AppName": "app-1",
-						"AppHost": "app-host-1",
+						"AppName": appName,
+						"AppHost": appHost,
 					}, nil)
 				},
 			}
@@ -216,6 +216,186 @@ var _ = Describe("BindService", func() {
 			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
 			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
 			Expect(redisClient.HMSetCalls()).To(HaveLen(1))
+		})
+	})
+
+	_ = Describe("UnbindApp", func() {
+		_ = It("indicates when instance does not exist", func() {
+			// arrange
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return nil, services.InstanceRetrievalNotFound
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindInstanceNotFound))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(0))
+			Expect(redisClient.DelCalls()).To(HaveLen(0))
+		})
+
+		_ = It("indicates when fails to check existing app binding", func() {
+			// arrange
+			instance := &models.Instance{
+				Name: instanceName,
+				Status: models.InstanceStatusRunning,
+			}
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return instance, services.InstanceRetrievalSuccess
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{
+				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
+					return redis.NewStringStringMapResult(nil, errors.New("some error"))
+				},
+			}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindFailure))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
+			Expect(redisClient.DelCalls()).To(HaveLen(0))
+		})
+
+		_ = It("indicates when can't find instance binding to app", func() {
+			// arrange
+			instance := &models.Instance{
+				Name: instanceName,
+				Status: models.InstanceStatusRunning,
+			}
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return instance, services.InstanceRetrievalSuccess
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{
+				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
+					return redis.NewStringStringMapResult(map[string]string{}, nil)
+				},
+			}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindNotBound))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
+			Expect(redisClient.DelCalls()).To(HaveLen(0))
+		})
+
+		_ = It("indicates when fails to remove binding", func() {
+			// arrange
+			instance := &models.Instance{
+				Name: instanceName,
+				Status: models.InstanceStatusRunning,
+			}
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return instance, services.InstanceRetrievalSuccess
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{
+				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
+					return redis.NewStringStringMapResult(map[string]string{
+						"AppName": appName,
+						"AppHost": appHost,
+					}, nil)
+				},
+				DelFunc: func(keys ...string) *redis.IntCmd {
+					return redis.NewIntResult(0, errors.New("some error"))
+				},
+			}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindFailure))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
+			Expect(redisClient.DelCalls()).To(HaveLen(1))
+		})
+
+		_ = It("indicates when does not find the binding when tries to remove it", func() {
+			// arrange
+			instance := &models.Instance{
+				Name: instanceName,
+				Status: models.InstanceStatusRunning,
+			}
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return instance, services.InstanceRetrievalSuccess
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{
+				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
+					return redis.NewStringStringMapResult(map[string]string{
+						"AppName": appName,
+						"AppHost": appHost,
+					}, nil)
+				},
+				DelFunc: func(keys ...string) *redis.IntCmd {
+					return redis.NewIntResult(0, nil)
+				},
+			}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindNotBound))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
+			Expect(redisClient.DelCalls()).To(HaveLen(1))
+		})
+
+		_ = It("indicates when removes the binding successfully", func() {
+			// arrange
+			instance := &models.Instance{
+				Name: instanceName,
+				Status: models.InstanceStatusRunning,
+			}
+			instanceService := &mocks.InstanceServiceMock{
+				GetByNameFunc: func(name string) (*models.Instance, services.InstanceRetrievalResult) {
+					return instance, services.InstanceRetrievalSuccess
+				},
+			}
+			redisClient := &mocks.UniversalClientMock{
+				HGetAllFunc: func(key string) *redis.StringStringMapCmd {
+					return redis.NewStringStringMapResult(map[string]string{
+						"AppName": appName,
+						"AppHost": appHost,
+					}, nil)
+				},
+				DelFunc: func(keys ...string) *redis.IntCmd {
+					return redis.NewIntResult(1, nil)
+				},
+			}
+			bindService := services.NewBindService(config, logger, redisClient, instanceService)
+
+			// act
+			result := bindService.UnbindApp(instanceName, appBindForm)
+
+			// assert
+			Expect(result).To(Equal(services.AppUnbindSuccess))
+			Expect(instanceService.GetByNameCalls()).To(HaveLen(1))
+			Expect(redisClient.HGetAllCalls()).To(HaveLen(1))
+			Expect(redisClient.DelCalls()).To(HaveLen(1))
 		})
 	})
 })
